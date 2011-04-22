0744d9ff7c069b8580b87f43cce3eef14998110c
ackepenek <ahmetcan196@gmail.com>
2011-04-22 13:01:57 +0000

    giris ekranı ile ilgili düzenleme yapıldı

--- oguz/resttest/myapp/models.py
+++ oguz/resttest/myapp/models.py
@@ -3,36 +3,44 @@ from django.db import models
 # Create your models here.
 
 class Sehir(models.Model):
-	adi = models.CharField(max_length=50)
+    adi = models.CharField(max_length=50)
 
 class Ulke(models.Model):
-	adi = models.CharField(max_length=50)
+    adi = models.CharField(max_length=50)
 
 class Konum(models.Model):
-	enlem = models.FloatField()
-	boylam = models.FloatField()
-	ulke = models.ForeignKey(Ulke)
-	sehir = models.ForeignKey(Sehir)
+    enlem = models.FloatField()
+    boylam = models.FloatField()
+    ulke = models.ForeignKey(Ulke)
+    sehir = models.ForeignKey(Sehir)
 
 class EgitimDurumu(models.Model):
-	adi = models.CharField(max_length=50)
+    adi = models.CharField(max_length=50)
 
 class IlgiAlanlari(models.Model):
-	adi = models.CharField(max_length=50)
+    adi = models.CharField(max_length=50)
 
 class Meslek(models.Model):
-	adi = models.CharField(max_length=50)
+    adi = models.CharField(max_length=50)
 
 class KullaniciDetaylari(models.Model):
-	meslek = models.ForeignKey(Meslek)
-	egitim_durumu = models.ForeignKey(EgitimDurumu)
-	ilgi_alanlari = models.ForeignKey(IlgiAlanlari)
+    meslek = models.ForeignKey(Meslek)
+    egitim_durumu = models.ForeignKey(EgitimDurumu)
+    ilgi_alanlari = models.ForeignKey(IlgiAlanlari)
 
 class Kullanici(models.Model):
-	ad = models.CharField(max_length=100)
-	soyad = models.CharField(max_length=100)
-	dogum_tarihi = models.DateTimeField()
-	email = models.EmailField()
-	parola = models.CharField(max_length=100)
-	konum = models.ForeignKey(Konum)
-	kullanici_detaylari = models.ForeignKey(KullaniciDetaylari)
+    dogrulama_id=models.CharField(max_length=100)
+    ad = models.CharField(max_length=100)
+    soyad = models.CharField(max_length=100)
+    dogum_tarihi = models.DateTimeField()
+    email = models.EmailField()
+    parola = models.CharField(max_length=100)
+    konum = models.ForeignKey(Konum)
+    kullanici_detaylari = models.ForeignKey(KullaniciDetaylari)
+    durum = models.BooleanField()
+class Kayit(models.Model):
+    ad = models.CharField(max_length=100)
+    soyad = models.CharField(max_length=100)
+    email = models.EmailField()
+    dogrulama_id = models.CharField(max_length=100)
+    kayit_tarihi = models.DateTimeField()

f701a2bab6c03a42bc8c6cff8415da5701c4c297
ackepenek <ahmetcan196@gmail.com>
2011-05-02 22:51:31 +0000

    mail gönderme işlemi entegre edildi

--- oguz/resttest/api/kullaniciHandler.py
+++ oguz/resttest/api/kullaniciHandler.py
@@ -1,13 +1,15 @@
+#ifndef KULLANICIHANDLER.PY
+#define KULLANICIHANDLER.PY
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from piston.handler import BaseHandler
 from piston.utils import rc,throttle
 from myapp.models import *
-
+from api.mailGonder import *
 class KullaniciHandler(BaseHandler):
     allowed_methods = ("GET", "PUT", "POST")
 
-    def read(self, request, kullanici_id):
+    def read(self, request,kullanici_id):
         if(kullanici_id):
             try:
                 return Kullanici.objects.get(id=kullanici_id)
@@ -16,6 +18,7 @@ class KullaniciHandler(BaseHandler):
         else:
             return Kullanici.objects.all()
     def uptade(self,request,kullanici_id):
+
         kullanici = Kullanici.objects.get(id = kullanici_id)
         kullanici.adi = request.PUT.get("ad")
         kullanici.soyad = request.PUT.get("soyad")
@@ -25,12 +28,15 @@ class KullaniciHandler(BaseHandler):
         kullanici.save()
         return rc.ALL_OK
     def create(self,request):
-        kullanici = Kullanici.objects.get_or_create(ad=request.POST.get("ad"),soyad=request.POST.get("soyad"),email=request.POST.get("email"))
-        return "sdvf"
+        kullanici,created = Kullanici.objects.get_or_create(ad=request.POST.get("ad"),
+                soyad=request.POST.get("soyad"),email=request.POST.get("email"))
         if kullanici.parola:
             return rc.CREATED
-        else:
-            kullanici.parolo = request.POST.get("parola"), 
-            kullanici.dogrulama_id = request.POST.get("dogrulama_id")
-            kullanici.save()
+        kullanici.parola = request.POST.get("parola")
+        kullanici.dogrulama_id = request.POST.get("dogrulama_id")
+        kullanici.save()
+        mail=MailGonderHandler()
+        mail.sender("Ahbap Onay Kodu",
+                    "Parolaniz: "+kullanici.parola+" Aktivasyon Linki: http://10.0.2.2:8000/api/aktiflestir/"+kullanici.dogrulama_id,"ahmetcan.kepenek@gmail.com")
         return rc.CREATED
+#endif // KULLANICIHANDLER.PY
